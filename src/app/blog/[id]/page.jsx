941d7963d4a906772975c2a1d52be1774e769750
Afsana Akhter Khusbu <hadirifat100@gmail.com>
2023-06-02 17:41:26 +0600

    setup route

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h2>The bog has an id.</h2>
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/dashboard/settings/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h2>This is settings page. </h2>
+        </div>
+    );
+};
+
+export default page;
