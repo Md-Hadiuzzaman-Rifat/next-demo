1a154d286e48e384b08d3d039f6a196a0231e581
Afsana Akhter Khusbu <hadirifat100@gmail.com>
2023-06-08 03:05:55 +0600

    done model

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
+import styles from "./page.module.css";
+import Image from "next/image";
+// import img from "./public/apps.jpg"
 
-const page = () => {
-    return (
-        <div>
-            <h2>This is blog page. </h2>
-        </div>
-    );
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data.");
+  }
+  return res.json();
+}
+
+const Blog = async () => {
+  const data = await getData();
+  return (
+    <div className={styles.container}>
+      {data.map((item) => 
+        <Link key={item.key} href={`/blog/${item.id}`}>
+          <div className="imgContainer">
+            <Image
+              src={'/apps.jpg'}
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+            <div className={styles.content}>
+              <h1 className={styles.title}>{item.title}</h1>
+              <p className={styles.desc}>{item.desc}</p>
+            </div>
+          </div>
+        </Link>
+      )}
+    </div>
+  );
 };
 
-export default page;
+export default Blog;
--- src/components/DarkMoodToggle/DarkMoodToggle.js
+++ src/components/DarkMoodToggle/DarkMoodToggle.js
@@ -8,8 +8,8 @@ const DarkMoodToggle = () => {
     return (
         <div>
             <div className={styles.container} onClick={toggle}>
-                <div className={styles.icon }></div>
-                <div className={styles.icon }></div>
+                <div className={styles.icon }>🌙</div>
+                <div className={styles.icon }>🔆</div>
                 <div className={styles.ball } style={mood==="light"?{left:"2px"}:{right:"2px"}} ></div>
             </div>
         </div>
