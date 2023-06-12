26988fd4d9f8292f981586bbc910738b6e717ee9
Afsana Akhter Khusbu <hadirifat100@gmail.com>
2023-06-12 16:02:47 +0600

    dashboard design done

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -6,7 +6,6 @@ export const GET=async(request)=>{
     try{
         await connect()
         const posts=await Post.find()
-
         return new NextResponse(JSON.stringify(posts),{status:200})
     }catch(err){
         return new NextResponse("Database Error",{status:500})
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,23 +1,60 @@
-"use client"
-import React from 'react';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+"use client";
+import React from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import useSWR from "swr";
+import styles from "./dashboard.module.css";
+import Image from "next/image";
 const Dashboard = () => {
-    const session=useSession()
-    const router=useRouter()
+  const session = useSession();
+  const router = useRouter();
 
-     if(session.status === "loading"){
-        return <p>Loading...</p>
-     }
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-    if (session.status === "unauthenticated"){
-        router?.push("/dashboard/login")
-    }
+  const { data, error, isLoading } = useSWR("/api/posts", fetcher);
 
-    if(session.status=== "authenticated"){
-        return <h2>Dashboard</h2>
-    }
+  if (session.status === "loading") {
+    return <p>Loading...</p>;
+  }
 
+  if (session.status === "unauthenticated") {
+    router?.push("/dashboard/login");
+  }
+
+  if (session.status === "authenticated") {
+    return (
+      <div>
+        <h1 className={styles.heading}>Dashboard</h1>
+        <div className={styles.container}>
+          <div className={styles.content}>
+            {!isLoading &&
+              data &&
+              data.map((post) => (
+                <div className={styles.post} key={post._id}>
+                  <div className={styles.imgContainer}>
+                    <Image src={post.img} alt="" width={200} height={100} />
+                  </div>
+                  <div className={styles.contentText}>
+                    <h2 className={styles.postTitle}>{post.title}</h2>
+                    <span className={styles.delete}>X</span>
+                  </div>
+                </div>
+              ))}
+          </div>
+          <form className={styles.form}>
+            <div className={styles.formPost}>
+                <input type="text" placeholder="Title"/>
+                <input type="text" placeholder="Description"/>
+                <input type="text" placeholder="Image"/>
+                <textarea placeholder="Content" id="" cols="30" rows="5"></textarea>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Dashboard;
+
+// onClick={() => handleDelete(post._id)
