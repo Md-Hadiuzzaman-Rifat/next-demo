824e4658098b42779df6d6d3aaf01cf9fc38f0f2
Afsana Akhter Khusbu <hadirifat100@gmail.com>
2023-06-22 03:19:00 +0600

    done about me section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./AboutMe.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const AboutMe = () => {
   return (
@@ -9,11 +11,28 @@ const AboutMe = () => {
         <h4>Me</h4>
       </div>
       <div className={styles.content}>
-        <div>
-
+        <div className={styles.content_image}>
+          <Image
+            className={styles.image}
+            src="/md_Hadiuzzaman_Rifat.JPG"
+            width={400}
+            height={500}
+            alt="photo of developer"
+          ></Image>
         </div>
-        <div>
-            
+        <div className={styles.content_description}>
+          <h1>Full Stack Web Developer</h1>
+          <p>
+            I am a &ldquo;Full Stack Web Developer&ldquo;. My Specialized area
+            is MERN Stack. I also use NextJs Framework. This project is made
+            with NextJs . If you need any information please follow my social
+            links bellow. Or if you interested to work with me, feel free to
+            contact me. I can make SEO friendly and dynamic website according to
+            client choice. Thank you Stay healthy, stay safe.
+          </p>
+          <Link  target="_blank" href="https://drive.google.com/drive/folders/1gjqloh3XjFKZ3iVfmOxIGbK-QBjVrPP8?usp=sharing">
+            <button>Download Resume</button>
+          </Link>
         </div>
       </div>
     </div>
